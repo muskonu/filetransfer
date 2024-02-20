f31531d64bc66b2921e6cec9d69ce2b2d6b8cc46
muskonu <22050918@hdu.edu.cn>
2024-02-20 11:13:59 +0800

    🦄 refactor: request unmarshal

--- signaling/proto/proto.go
+++ signaling/proto/proto.go
@@ -2,6 +2,7 @@ package proto
 
 import (
 	"encoding/json"
+
 	"github.com/go-playground/validator/v10"
 	"github.com/sirupsen/logrus"
 	"github.com/tidwall/gjson"
@@ -51,12 +52,13 @@ func NewResponse(code int64, message string) []byte {
 
 func PayloadRequest(payload []byte) (*Request, error) {
 	request := &Request{}
-	r := gjson.GetManyBytes(payload, "command", "source", "target", "body")
-	request.Cmd = r[0].Int()
-	request.Source = r[1].String()
-	request.Target = r[2].String()
-	request.Body = r[3].String()
-	err := validate.Struct(request)
+	err := json.Unmarshal(payload, request)
+	if err != nil {
+		return nil, err
+	}
+	r := gjson.GetBytes(payload, "body")
+	request.Body = r.String()
+	err = validate.Struct(request)
 	if err != nil {
 		logrus.Errorf("parse payloadRequest error: %v requset:%s", err, string(payload))
 		return nil, err
@@ -66,10 +68,11 @@ func PayloadRequest(payload []byte) (*Request, error) {
 
 func PayloadRegisterRequest(payload []byte) (*RegisterRequest, error) {
 	initRequest := &RegisterRequest{}
-	r := gjson.GetManyBytes(payload, "command", "source")
-	initRequest.Cmd = r[0].Int()
-	initRequest.SourceID = r[1].String()
-	err := validate.Struct(initRequest)
+	err := json.Unmarshal(payload, initRequest)
+	if err != nil {
+		return nil, err
+	}
+	err = validate.Struct(initRequest)
 	if err != nil {
 		return nil, err
 	}
